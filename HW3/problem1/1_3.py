24affc0f0930793fb2424282c3c107d8dea99a77
XiangPan <xiangpan@nyu.edu>
2022-03-26 18:56:44 -0400

    HW3 init

---
+++ HW3/problem1/1_3.py
@@ -0,0 +1,112 @@
+'''
+Author: Xiang Pan
+Date: 2022-03-26 16:51:35
+LastEditTime: 2022-03-26 17:40:20
+LastEditors: Xiang Pan
+Description: 
+FilePath: /HW3/problem1/1_3.py
+@email: xiangpan@nyu.edu
+'''
+import argparse
+import pytorch_lightning as pl
+import torch.nn as nn
+import torch
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import wandb
+from pytorch_lightning.loggers import WandbLogger
+
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, use_dropout=False):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.2)
+
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.droup2 = nn.Dropout(0.5)
+
+        self.use_dropout = use_dropout
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = x.view(batch_size, -1, self.fc1.in_features)
+        if self.use_dropout:
+            x = self.fc1(x)
+            x = self.relu1(x)
+            x = self.dropout1(x)
+            x = self.fc2(x).squeeze()
+            x = self.droup2(x)
+        else:
+            x = self.fc1(x)
+            x = self.relu1(x)
+            x = self.fc2(x).squeeze()
+        return x
+
+class MLPLightningModule(pl.LightningModule):
+    def __init__(self, hidden_size, output_size, optimizer_name='adam', use_dropout=False):
+        super(MLPLightningModule, self).__init__()
+        self.model = MLP(input_size=3*32*32, hidden_size=hidden_size, output_size=output_size, use_dropout=use_dropout)
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.optimizer_name = optimizer_name
+        
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        # print(y_hat.shape)
+        loss = self.loss_fn(y_hat, y)
+        self.log('train_loss', loss, prog_bar=True, on_epoch=True)
+        logs = {'train_loss': loss}
+        return {'loss': loss, 'log': logs}
+
+    def train_dataloader(self):
+        train_dataset = datasets.CIFAR10(root='./cached_datasets/CIFAR10', train=True, download=True, transform=transforms.ToTensor())
+        return torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=20)
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = self.loss_fn(y_hat, y)
+        return {'val_loss': loss}
+    def validation_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        logs = {'val_loss': avg_loss}
+        return {'val_loss': avg_loss, 'log': logs}
+
+    def configure_optimizers(self):
+        # print(self.optimizer_name, self.optimizer_name == "RMSprop")
+        if self.optimizer_name == 'Adagrad':
+            optimizer = torch.optim.Adagrad(self.parameters(), lr=0.001)
+        if self.optimizer_name == "RMSprop":
+            optimizer = torch.optim.RMSprop(self.parameters(), lr=0.001)
+        if self.optimizer_name == 'RMSprop+Nesterov':
+            optimizer = torch.optim.RMSprop(self.parameters(), lr=0.001, momentum=0.9)
+        if self.optimizer_name == 'Adadelta':
+            optimizer = torch.optim.Adadelta(self.parameters(), lr=0.001)
+        if self.optimizer_name == 'Adam':
+            optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use_dropout', action='store_true')
+    parser.add_argument('--optimizer_name', type=str, default="Adagrad")
+    args = parser.parse_args()
+    run = wandb.init(group='1.3')
+    run.display(height=720)
+    optimizer_name = args.optimizer_name
+
+    log_name = optimizer_name
+    if args.use_dropout:
+        log_name += "+Dropout"
+
+    wandb.run.name = log_name
+    logger = WandbLogger(project='HW3_1', save_dir="./outputs", name=log_name)
+    trainer = pl.Trainer(gpus=1, max_epochs=200, logger=logger)
+    model = MLPLightningModule(hidden_size=1000, output_size=10, optimizer_name=optimizer_name, use_dropout=args.use_dropout)
+    trainer.fit(model)
