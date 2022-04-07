49a2c0d01798d55f055ad8bab1103e4716c24f83
XiangPan <xiangpan@nyu.edu>
2022-04-07 03:32:54 -0400

    HW4 2_a

--- HW4/problem2/2_a.sh
+++ HW4/problem2/2_a.sh
@@ -2,7 +2,7 @@
  # @Created by: Xiang Pan
  # @Date: 2022-04-06 20:01:21
  # @LastEditors: Xiang Pan
- # @LastEditTime: 2022-04-06 20:09:22
+ # @LastEditTime: 2022-04-06 20:47:31
  # @Email: xiangpan@nyu.edu
  # @FilePath: /HW4/problem2/2_a.sh
  # @Description: 
@@ -12,6 +12,6 @@
 FILE="$(basename -- $0)"
 NAME=`echo "$FILE" | cut -d'.' -f1`
 EXTENSION=`echo "$FILE" | cut -d'.' -f2`
-python problem1/1_a.py --group $NAME --gpus 3 --lr 0.1 --fix_backbone &
-python problem1/1_a.py --group $NAME --gpus 4 --lr 0.001 --fix_backbone &
-python problem1/1_a.py --group $NAME --gpus 5 --lr 0.0001 --fix_backbone &
+python problem1/1_a.py --group $NAME --gpus 0 --lr 0.1 --fix_backbone &
+python problem1/1_a.py --group $NAME --gpus 0 --lr 0.001 --fix_backbone &
+python problem1/1_a.py --group $NAME --gpus 0 --lr 0.0001 --fix_backbone &
