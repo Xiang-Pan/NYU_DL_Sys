d8836322774b647e60da5ea981a98ffe1994ce92
XiangPan <xiangpan@nyu.edu>
2022-04-22 22:25:42 -0400

    update

--- HW3/make_submissions.sh
+++ HW3/make_submissions.sh
@@ -2,7 +2,7 @@
  # @Created by: Xiang Pan
  # @Date: 2022-04-06 03:47:46
  # @LastEditors: Xiang Pan
- # @LastEditTime: 2022-04-06 04:01:50
+ # @LastEditTime: 2022-04-12 01:01:53
  # @Email: xiangpan@nyu.edu
  # @FilePath: /HW3/make_submissions.sh
  # @Description: 
@@ -12,4 +12,6 @@ find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 cp problem*.* submissions/
 cp -r problem* submissions/
 cp -r models submissions/
+cp -r *.ipynb submissions/
+cp -r *.pdf submissions/
 zip -r Intro_DL_Sys_HW3_xp2030.zip submissions/
